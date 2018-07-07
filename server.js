0f4770a6323885fb0ccad8582d7bcfe25970b34c
sergio-peotta <sergio.peotta@gmail.com>
2018-07-07 12:37:00 +0200

    Header - Footer

--- server.js
+++ server.js
@@ -1,27 +1,47 @@
 const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = now + ':' + req.method + ' ' + req.url;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err) { console.log('Unable to append to server.log : ' + err); }
+    });
+    next();
+});
+
+// app.use((req, res, next) => { res.render('maintenance.hbs'); });
+
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (message) => {
+    return message.toUpperCase();
+});
+
 app.get('/', (req, res) => {
-    res.send({
-        name: 'Sergio',
-        firstName: 'Peotta',
-        likes: [
-            'Biking',
-            'Cities'
-        ]
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        currentYear: new Date().getFullYear(),
+        welcomeMessage: 'Welcome to my website'
     });
 });
 
 app.get('/about', (req, res) => {
-    res.send('About Page');
-});
-
-app.get('/bad', (req, res) => {
-    res.send({
-        errorMessage: 'Unable to handle request'
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear(),
+        welcomeMessage: 'This is About page.'
     });
 });
 
